c04e3fb7419ff8aa34775211d1815f649593e665
Sergey-Pashkov <Pashkov-sa@ya.ru>
2024-07-24 10:37:39 +0700

    Добавлены стили к странице входа

--- Home_button/Accounting_button/views.py
+++ Home_button/Accounting_button/views.py
@@ -1,7 +1,9 @@
 
+
+
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm
 
 def login_view(request):
     form = AuthenticationForm()
-    return render(request, 'Accounting_button/templates/login.html', {'form': form})
+    return render(request, 'Accounting_button/registration/login.html', {'form': form})
